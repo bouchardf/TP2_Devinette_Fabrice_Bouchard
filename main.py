fa28b6d17ab8331465c6dfc534098a36b3d605fb
bouchardf <bouchardf@educ.college-montreal.qc.ca>
2022-09-29 10:38:22 -0400

    Ajout de commentaire

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+#Jeu de devinette
+#29 septembre 2022
+#Créé par Fabrice Bouchard
+
 from random import*
+
+#Défnition des variables
 
 nb_essai = 0
 fin_jeu = False
@@ -10,26 +16,27 @@ print("J’ai choisi un nombre au hasard entre 0 et 1000. "
 
 nombre_aleatoire = randint(0, 1000)
 
+#Mise en place de la boucle dans laquelle le jeu prend place
 while fin_jeu == False:
     nb_joueur = int(input("Entrez votre essai :_"))
     nb_essai = nb_essai + 1
 
-    if nb_joueur > nombre_aleatoire:
+    if nb_joueur > nombre_aleatoire:        #Si le nombre est plus petit
         print("Mauvais choix, le nombre est plus petit que" ,nb_joueur,)
 
 
-    elif nb_joueur < nombre_aleatoire:
+    elif nb_joueur < nombre_aleatoire:      #Si le nombre est plus grand
         print("Mauvaise réponse, le nombre est plus grand que" ,nb_joueur,)
 
-    elif nb_joueur == nombre_aleatoire:
+    elif nb_joueur == nombre_aleatoire:     #Si l'essai est le bon
         print("Bravo! Bonne réponse! Vous avez réussi en:", nb_essai, "essais")
 
         rejouer = input("Voulez rejouer? Entrer n pour quitter et o pour continuer.")
 
-        if rejouer == "n":
+        if rejouer == "n":      #Si le joueur veut rejouer
             print("Merci et au revoir...")
             fin_jeu = True
 
-        elif rejouer == "o":
+        elif rejouer == "o":       #Si le joueur veut quitter
             nb_essai = 0
             nombre_aleatoire = randint(0, 1000)
